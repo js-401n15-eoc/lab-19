b0f42763824b63f4fbee1c7e8931a3beb44a1518
ecaoile <unfie555@gmail.com>
2020-03-02 16:58:04 -0800

    pushing up work done so far

--- queue-server.js
+++ queue-server.js
@@ -2,7 +2,30 @@
 
 require('dotenv').config();
 const io = require('socket.io')(process.env.PORT || 3000);
+const uuid = require('uuid').v4();
 
+let messages = {};
 io.of('db', socket => {
   console.log('welcome to the db channel', socket.id);
+  
+  socket.on('subscribe', payload => {
+    const {event, id} = payload;
+    if (!messages[event]) { messages[event] = {}; }
+    if (!messages[event][id]) { messages[event][id] = {}; }
+
+    console.log(event);
+    console.log(id);
+  });
+
+  socket.on('testEvent', payload => {
+    let event = 'testEvent';
+    for (let subscriber in messages['testEvent']) {
+      messages[event][subscriber][messageID] = payload;
+    }
+    io.of('db').emit('testEvent', payload);
+  });
+
+  socket.on('received', payload => {
+    console.log('ack', payload);
+  })
 });
---
+++ subscriber.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const io = require('socket.io-client');
+
+const socket = io.connect('http://localhost:3000/db');
+const clientID = 'bobTheTester';
+
+socket.emit('subscribe', { id: clientID, event: 'testEvent' });
+
+socket.on('testEvent', payload => {
+  socket.emit('received', true);
+  console.log('TE', payload);
+})
